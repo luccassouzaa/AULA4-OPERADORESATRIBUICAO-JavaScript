cd5fee2de2c74dbe28c2e47247ccbf40b1877d77
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-14 00:02:30 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,34 @@
+// Numbers //
+
+    var x = 10
+    var y = 2
+
+    // x = x + y
+
+    x += y
+    console.log(x)
+
+    x -= y
+    console.log(x)
+
+    x /= y
+    console.log(x)
+
+    x *= y
+    console.log(x)
+
+    x %= y
+    console.log(x)
+
+// String //
+
+    var produto = ["camisa", "calça"]
+    var text = "Temos em estoque: "
+
+    text += produto[1] + ', '
+    text += produto[0] + ''
+
+
+    console.log(text + '.')
+
+
